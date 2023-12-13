de48f965b2d620c8a9e51dde517f2945ed72de5c
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-14 01:35:19 +0800

    update: added headers for cors

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 //APIs for Authentication
 // Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register']);
