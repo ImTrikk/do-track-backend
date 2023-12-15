50d6e597cde0d5f34c741e2588db5cea0b1a39ba
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 01:35:40 +0800

    update added cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ["https://do-track.vercel.app/"],
 
     'allowed_headers' => ['*'],
 
--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('tokenable_id')->unique();
+            $table->string('tokenable_id');
             $table->string('tokenable_type');
             $table->string('name');
             $table->string('token', 64)->unique();
