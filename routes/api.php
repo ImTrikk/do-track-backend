90be7a893fbbffe0a63d507c6187fc2a0ceca746
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 02:38:34 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,9 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-header('Access-Control-Allow-Origin: https://do-track.vercel.app');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+// header('Access-Control-Allow-Origin: https://do-track.vercel.app');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 
 
 //APIs for Authentication
