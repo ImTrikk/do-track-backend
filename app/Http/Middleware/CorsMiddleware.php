fec127ce1d9e2cf439d26eb0228d689df6023157
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 17:03:03 +0800

    udpate cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,22 +8,12 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        dd('test');
         $response = $next($request);
 
-        $allowedOrigins = [
-            '*',
-            // Add more allowed origins if needed
-        ];
-
-        $origin = $request->headers->get('Origin');
-
-        if (in_array($origin, $allowedOrigins)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-            $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
+        $response->header(
+            'Access-Control-Allow-Origin',
+            'https://do-track.vercel.app/signup'
+        );
 
         return $response;
     }
