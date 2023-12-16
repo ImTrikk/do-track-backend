c4f4d6fe8577d81ea7df5857a5685606f6903f50
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 23:12:42 +0800

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,10 +10,16 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->header(
-            'Access-Control-Allow-Origin',
-            '*'
-        );
+        // Set CORS headers for the actual request
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // Handle preflight requests
+        if ($request->isMethod('OPTIONS')) {
+            $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            return $response;
+        }
 
         return $response;
     }
