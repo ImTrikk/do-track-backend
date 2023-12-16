fac07d435ea5fe48ab4853a17a0e22099b39ee3b
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 21:24:49 +0800

    update, remoge corsMiddleware

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,10 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*', 'https://do-track.vercel.app'],
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
