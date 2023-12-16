2bfa3e4e71fec5da6b9a10148c90e85f2686e08f
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 01:42:08 +0800

    udpaet preflight

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -14,9 +15,14 @@ class PreflightResponse
      */
     public function handle($request, Closure $next)
     {
-        if ($request->getMethod() === "OPTIONS") {
-            return response('', 200);
+        // Handle preflight OPTIONS requests
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 204)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         }
+
         return $next($request);
     }
 }
