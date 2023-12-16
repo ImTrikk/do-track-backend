64d9dae3061de9ab8ec3fafbd3b8cfa4ff5e2a15
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 21:37:34 +0800

    update

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 // config/cors.php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
