22611d3d687e5c31b61317a1e376a6e09bcfe0a1
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 22:13:07 +0800

    udpate cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://do-track.vercel.app/register', 'https://do-track.vercel.app/login'],
 
     'allowed_origins_patterns' => [],
 
