1ca2da2c319af9aad4e858a6a8e67996a19a326a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 22:03:53 +0800

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,10 +8,13 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
-    }
+        $response = $next($request);
 
+        $response->header(
+            'Access-Control-Allow-Origin',
+            'https://do-track.vercel.app/'
+        );
+
+        return $response;
+    }
 }
