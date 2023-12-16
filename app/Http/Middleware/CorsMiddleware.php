a7125aa48d52083b3fadb8e9a154c415343f730a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 19:46:10 +0800

    updated cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -11,7 +11,7 @@ class CorsMiddleware
         $response = $next($request);
 
         $headers = [
-            'Access-Control-Allow-Origin' => 'https://do-track.vercel.app',
+            'Access-Control-Allow-Origin' => '*',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
             'Access-Control-Allow-Credentials' => 'true',
