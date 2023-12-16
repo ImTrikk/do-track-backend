246608ac4a9cda9714ddb94d5f1c8d984ddc7a6b
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 18:08:11 +0800

    update auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 
-use App\Models\Admin;
 use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\Models\Admins;
+use App\Models\Admin;
 
 
 
