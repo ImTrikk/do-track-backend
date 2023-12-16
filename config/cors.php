26328fd816d9d146053518bc3ed586f2f6b93a0c
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 00:58:01 +0800

    update cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [],
 
     'allowed_origins' => ['https://do-track.vercel.app'],
 
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-// header('Access-Control-Allow-Origin');
+header('Access-Control-Allow-Origin: https://do-track.vercel.app');
 
 //APIs for Authentication
 // Route::group(['prefix' => 'auth'], function () { 
