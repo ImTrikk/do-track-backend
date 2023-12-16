914cff75d51fd82082c521b57a50fe165f1d6b9a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-16 21:00:24 +0800

    update corsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,18 +8,10 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
-            'Access-Control-Allow-Credentials' => 'true',
-        ];
-
-        $response->headers->add($headers);
-
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'PUT', "PUT, POST, DELETE, GET, OPTIONS")
+            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
     }
 
 }
