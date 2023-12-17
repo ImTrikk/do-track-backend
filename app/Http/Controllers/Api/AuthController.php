3b4741c6707ec71ddc54e3de06df82e474005ae4
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 09:46:55 +0800

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,7 +45,10 @@ class AuthController extends Controller
                     'data' => $admin,
                 ],
                 201
-            );
+            )->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         } else {
             return response()->json(
                 [
--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,19 +8,13 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // If it's a preflight request, respond immediately
-        if ($request->isMethod('OPTIONS')) {
-            return response()
-                ->json([], 200)
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        }
+        // Log or debug statement to verify middleware execution
+        \Log::info('CorsMiddleware executed.');
 
-        // Continue with the request
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
+
 }
