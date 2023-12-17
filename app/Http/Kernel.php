347a8a477da2ae875275739965e4abc5f015764f
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 09:56:45 +0800

    update kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,18 +19,20 @@ class Kernel extends HttpKernel
 
     protected $middlewareGroups = [
         'web' => [
+                // Other web middlewares...
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-                // \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-                // Remove the following line
             \App\Http\Middleware\CorsMiddleware::class,
+            // Other web middlewares...
         ],
 
         'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
