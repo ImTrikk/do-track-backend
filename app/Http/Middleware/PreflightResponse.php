c0b17f84c43aeea300269c6bda940997b3870a6d
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 18:12:19 +0800

    udpate

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -18,7 +18,7 @@ class PreflightResponse
         // Handle preflight OPTIONS requests
         if ($request->isMethod('OPTIONS')) {
             return response()
-                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Origin', 'https://do-track.vercel.app')
                 ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
                 ->header('Access-Control-Allow-Credentials', 'true');
