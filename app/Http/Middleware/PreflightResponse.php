9c1ba6f5d63a9d83b20e5bf51790ce11299a766d
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 18:43:54 +0800

    update

--- app/Http/Middleware/PreflightResponse.php
+++ app/Http/Middleware/PreflightResponse.php
@@ -18,11 +18,11 @@ class PreflightResponse
         // Handle preflight OPTIONS requests
         if ($request->isMethod('OPTIONS')) {
             return response()
-                ->setStatusCode(200) // Set your desired status code here
                 ->header('Access-Control-Allow-Origin', 'https://do-track.vercel.app')
                 ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-                ->header('Access-Control-Allow-Credentials', 'true');
+                ->header('Access-Control-Allow-Credentials', 'true')
+                ->setStatusCode(200); // Set your desired status code here
         }
         return $next($request);
     }
