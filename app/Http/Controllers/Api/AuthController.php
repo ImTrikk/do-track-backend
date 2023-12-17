244e25d532976a242a66d3c2f81ad7d722b946f5
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-17 10:11:15 +0800

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,10 +45,7 @@ class AuthController extends Controller
                     'data' => $admin,
                 ],
                 201
-            )->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
+            );
         } else {
             return response()->json(
                 [
--- config/cors.php
+++ config/cors.php
@@ -15,10 +15,10 @@ return [
     |
     */
 
-    'paths' => ["*"],
+    'paths' => ["*", 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ["*"],
-    
+
     'allowed_origins' => ["*"],
 
     'allowed_origins_patterns' => ["*"],
