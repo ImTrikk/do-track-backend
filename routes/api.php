fef2f917734874f8cb9691e2fa328faa0a837dfd
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-20 12:45:32 +0800

    removed api from sanctum

--- routes/api.php
+++ routes/api.php
@@ -27,47 +27,47 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    // attendance api routes
-    Route::controller(AttendanceController::class)->group(function () {
+});
+// attendance api routes
+Route::controller(AttendanceController::class)->group(function () {
 
-        // api route for time_in and time_out of students
-        Route::post('/attendance/record-time', 'RecordTime');
+    // api route for time_in and time_out of students
+    Route::post('/attendance/record-time', 'RecordTime');
 
-        // api route for getting the 
-        Route::get('/attendance/college-info/{id}', 'getCollegeInfo');
+    // api route for getting the 
+    Route::get('/attendance/college-info/{id}', 'getCollegeInfo');
 
-        // api router for getting the students, filtered by college_id
-        Route::get('/attendance/attendance-by-college/{id}', 'getAttendanceByCollege');
+    // api router for getting the students, filtered by college_id
+    Route::get('/attendance/attendance-by-college/{id}', 'getAttendanceByCollege');
 
-        // api router for getting the students, filtered by program_id
-        Route::get('/attendance/attendance-by-program/{id}', 'getAttendanceByProgram');
+    // api router for getting the students, filtered by program_id
+    Route::get('/attendance/attendance-by-program/{id}', 'getAttendanceByProgram');
 
-        // api router for getting the students with completed rendered hours  (3hours)
-        Route::get('/attendance/attendance-by-rendered-hours', 'getAttendanceByRenderedHour');
+    // api router for getting the students with completed rendered hours  (3hours)
+    Route::get('/attendance/attendance-by-rendered-hours', 'getAttendanceByRenderedHour');
 
-        // api router for getting the students with completed rendered hours  (3hours), filteered with college_id
-        Route::get('/attendance/attendance-by-rendered-hours-college/{id}', 'getAttendanceByRenderedHourWithCollege');
+    // api router for getting the students with completed rendered hours  (3hours), filteered with college_id
+    Route::get('/attendance/attendance-by-rendered-hours-college/{id}', 'getAttendanceByRenderedHourWithCollege');
 
-        // api router for getting the students with no time_in and time_out
-        Route::get('/attendance/attendance-by-no-timestamps/{id}', 'getStudentNoTimeInOut');
+    // api router for getting the students with no time_in and time_out
+    Route::get('/attendance/attendance-by-no-timestamps/{id}', 'getStudentNoTimeInOut');
 
-        // api route for atendees
-        Route::get('/attendance/get-student-attendees/{id}', 'getStudentAttendees');
+    // api route for atendees
+    Route::get('/attendance/get-student-attendees/{id}', 'getStudentAttendees');
 
-        // get all scanned students by a specific admin
-        Route::get('/attendance/get-scanned-by-admin/{id}', "getScannedByAdmin");
+    // get all scanned students by a specific admin
+    Route::get('/attendance/get-scanned-by-admin/{id}', "getScannedByAdmin");
 
-        // delete student attendance record
-        Route::delete('/attendance/delete-student-record/{id}', 'deleteStudentAttendanceRecord');
+    // delete student attendance record
+    Route::delete('/attendance/delete-student-record/{id}', 'deleteStudentAttendanceRecord');
 
-        // api for finding student_id
-        Route::get('/attendance/find-student/{id}', 'findStudentId');
-    });
+    // api for finding student_id
+    Route::get('/attendance/find-student/{id}', 'findStudentId');
+});
 
 
-    // route api for CollegeController
-    Route::controller(CollegeController::class)->group(function () {
-        // gets all the program information in college
-        Route::get('/college/get-programs-college/{id}', 'getProgramsByCollege');
-    });
+// route api for CollegeController
+Route::controller(CollegeController::class)->group(function () {
+    // gets all the program information in college
+    Route::get('/college/get-programs-college/{id}', 'getProgramsByCollege');
 });
